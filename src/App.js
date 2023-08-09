a875938e54c0f08872fe959fb6ca37d13b382493
Shikuku39 <enshikuku39@gmail.com>
2023-08-09 13:19:32 +0300

    lesson 2 react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Job salary = {30000} job = 'Teacher' company = 'St. Anthony' />
-            <Job salary = {10000} job = 'Cook' company = 'Guardian' />
-            <Job salary = {40000} job = 'Head Teacher' company = 'Hurumaa' />
-        </div>
-    );
-}
-
-const Job = (props) => {
-    return (
-        <div>
-            <h1>{props.salary}</h1>
-            <h1>{props.job}</h1>
-            <h1>{props.company}</h1>
-        </div>
-    )
+    const age = 150
+    return <div className='App'> {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}</div>
+    
 }
 
 export default App;
